2e91526dce47095f11eba0e881447b2a1615ca20
v.adhithyan <aavispeaks@gmail.com>
2016-07-18 11:08:28 +0530

    after submit entry form, go to details view for chosen date's week'

--- app/src/main/java/com/jonathenchen/paintracker/listeners/DateListener.java
+++ app/src/main/java/com/jonathenchen/paintracker/listeners/DateListener.java
@@ -3,12 +3,15 @@ package com.jonathenchen.paintracker.listeners;
 import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.telecom.Call;
+import android.util.Log;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
 import com.jonathenchen.paintracker.utilites.DateUtil;
+import com.jonathenchen.paintracker.utilites.DetailsUtil;
 import com.jonathenchen.paintracker.utilites.EntryFormUtil;
 
 import java.util.Calendar;
@@ -32,6 +35,7 @@ public class DateListener implements View.OnClickListener {
         int year = cal.get(Calendar.YEAR);
         final int month = cal.get(Calendar.MONTH);
         int day = cal.get(Calendar.DAY_OF_MONTH);
+        final int dayOfYear = cal.get(Calendar.DAY_OF_YEAR);
 
         DatePickerDialog datePickerDialog = new DatePickerDialog(activity, new DatePickerDialog.OnDateSetListener() {
             @Override
@@ -40,6 +44,13 @@ public class DateListener implements View.OnClickListener {
                 newCal.set(Calendar.DAY_OF_MONTH, dayOfMonth);
                 newCal.set(Calendar.YEAR, year);
                 newCal.set(Calendar.MONTH, monthOfYear);
+
+                int chosenDayOfYear = newCal.get(Calendar.DAY_OF_YEAR);
+                int diff = (dayOfYear - chosenDayOfYear) / 7;
+                diff = diff + 1;
+                DetailsUtil.weekIncrement = diff;
+
+
                 String dateChosen = new DateUtil().getDate(newCal);
                 date.setText(dateChosen);
                 EntryFormUtil.date = dateChosen;
